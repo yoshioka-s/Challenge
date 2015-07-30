8bf4c351433ec43f9e11fa4b2e960c7e040f134d
Shu Yoshioka <yoshioka63@gmail.com>
2015-07-31 05:48:27 +0900

    [bug-fix] participants

--- public/js/create.js
+++ public/js/create.js
@@ -7,19 +7,22 @@ angular.module('challengeApp.createChallenge', ['isteven-multi-select'])
   $scope.selectedParticipant = null;
 
 
-  $scope.modernBrowsers = [
-    { icon: "<img src='../img/placeholder.jpg'/>",            name: "Bob",                 ticked: false  },
-    { icon: "<img src='../img/placeholder.jpg'/>",            name: "Peter",               ticked: false },
-    { icon: "<img src='../img/placeholder.jpg'/>",            name: "Marry",               ticked: false  },
-    { icon: "<img src='../img/placeholder.jpg'/>",            name: "Alex",                ticked: false },
-    { icon: "<img src='../img/placeholder.jpg'/>",            name: "John",                ticked: false  }
-  ];
+  // $scope.modernBrowsers = [
+  //   { icon: "<img src='../img/placeholder.jpg'/>",            name: "Bob",                 ticked: false  },
+  //   { icon: "<img src='../img/placeholder.jpg'/>",            name: "Peter",               ticked: false },
+  //   { icon: "<img src='../img/placeholder.jpg'/>",            name: "Marry",               ticked: false  },
+  //   { icon: "<img src='../img/placeholder.jpg'/>",            name: "Alex",                ticked: false },
+  //   { icon: "<img src='../img/placeholder.jpg'/>",            name: "John",                ticked: false  }
+  // ];
 
   // get array of all users in the database
   CreateChallengeFactory.getAllUsers().then(function(res){
-     $scope.allUsers= res.filter(function(user) {
+     $scope.allUsers = res.filter(function(user) {
       console.log(user)
       return (user.id !== $scope.user.id);
+    });
+    $scope.modernBrowsers = $scope.allUsers.map(function (user) {
+      return {icon: "<img src='../img/placeholder.jpg'/>"/* TODO use user.profile_image */, name: user.username, ticked: false};
     });
      console.log($scope.allUsers);
   });
@@ -33,13 +36,13 @@ angular.module('challengeApp.createChallenge', ['isteven-multi-select'])
 
   // method that takes the challengeInfo object as argument and calls the factory POST call
   $scope.postChallenge = function(){
-    console.log('id is',$scope.user.id);
     if ($scope.user.coin < $scope.challengeInfo.wager) {
       console.log('the wager exceeds your coin');
       $scope.errorMessage = 'make sure the wager does not exceed your coin.';
       return;
     }
-    CreateChallengeFactory.postChallenge($scope.challengeInfo, $scope.use.id).then(function(res){
+    console.log($scope.challengeInfo.participants);
+    CreateChallengeFactory.postChallenge($scope.challengeInfo).then(function(res){
       $state.go('challenge_view', {'id': res.id});
     });
   };
