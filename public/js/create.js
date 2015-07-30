67df249bc725bfd8c2742ecc16fd914692c24a87
chuqidai <daichuqi@gmail.com>
2015-07-30 15:43:09 -0700

    [db]update create

--- public/js/create.js
+++ public/js/create.js
@@ -1,50 +1,46 @@
 angular.module('challengeApp.createChallenge', ['isteven-multi-select'])
 .controller('CreateChallengeController', function ($scope, $state, CreateChallengeFactory) {
 
-  $scope.allUsers = [];
+  $scope.participants = [];
   $scope.challengeInfo = {};
   $scope.challengeInfo.participants = [];
   $scope.selectedParticipant = null;
 
 
-  $scope.participants = [
-    { icon: "<img src='../img/placeholder.jpg'/>",            name: "Bob",                 ticked: false  },
-    { icon: "<img src='../img/placeholder.jpg'/>",            name: "Peter",               ticked: false },
-    { icon: "<img src='../img/placeholder.jpg'/>",            name: "Marry",               ticked: false  },
-    { icon: "<img src='../img/placeholder.jpg'/>",            name: "Alex",                ticked: false },
-    { icon: "<img src='../img/placeholder.jpg'/>",            name: "John",                ticked: false  }
-  ];
+  // $scope.participants = [
+  //   { icon: "<img src='../img/placeholder.jpg'/>",            name: "Bob",                 ticked: false },
+  //   { icon: "<img src='../img/placeholder.jpg'/>",            name: "Peter",               ticked: false },
+  //   { icon: "<img src='../img/placeholder.jpg'/>",            name: "Marry",               ticked: false },
+  //   { icon: "<img src='../img/placeholder.jpg'/>",            name: "Alex",                ticked: false },
+  //   { icon: "<img src='../img/placeholder.jpg'/>",            name: "John",                ticked: false }
+  // ];
 
   // get array of all users in the database
   CreateChallengeFactory.getAllUsers().then(function(res){
-     $scope.allUsers= res.filter(function(user) {
-      console.log(user)
-      return (user.id !== $scope.user.id);
+    res.filter(function(user) {
+      user.ticked = false;
+      $scope.participants.push(user)
     });
      console.log($scope.allUsers);
   });
 
-  $scope.addParticipant = function() {
-    console.log($scope.user);
-    if ($scope.challengeInfo.participants.indexOf($scope.selectedParticipant.id) === -1) {
-      $scope.challengeInfo.participants.push($scope.selectedParticipant);
-    }
-  };
+  $scope.postChallenge = function(challengeInfo){
+    challengeInfo.title = challengeInfo.title|| "untitled";
+    challengeInfo.time = challengeInfo.time || "300";
+    challengeInfo.wager = challengeInfo.wager || "100";
+    challengeInfo.message = challengeInfo.message || "no description";
 
-  // method that takes the challengeInfo object as argument and calls the factory POST call
-
-  $scope.postChallenge = function(){
-    console.log('id is',$scope.user.id);
+    console.log($scope.user)
     if ($scope.user.coin < $scope.challengeInfo.wager) {
       console.log('the wager exceeds your coin');
       $scope.errorMessage = 'make sure the wager does not exceed your coin.';
       return;
-    }
-
-    CreateChallengeFactory.postChallenge($scope.challengeInfo, $scope.use.id).then(function(res){
-      $state.go('challenge_view', {'id': res.id});
-    });
-  };
+  }
+  CreateChallengeFactory.postChallenge(challengeInfo).then(function(res){
+      // $state.go('challenge_view', {'id': res.id});
+      console.log(res);
+  });
+};
 
 
 });
