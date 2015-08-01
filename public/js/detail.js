82cc3fb6f442ae9068da662074a720e800601bdb
chuqidai <daichuqi@gmail.com>
2015-07-31 17:25:39 -0700

    [add]challenge image

--- public/js/detail.js
+++ public/js/detail.js
@@ -1,6 +1,7 @@
 angular.module('challengeApp.detail', [])
 .controller('DetailController', ['$scope','$location','$state','$stateParams', 'ChallengeFactory','UserFactory',
   function($scope,$location,$state,$stateParams,ChallengeFactory,UserFactory){
+
     $scope.challenge = $stateParams.itemId;
     $scope.userChallenges = [];
     $scope.getChallengeInfo = function(id,callback) {
@@ -25,13 +26,14 @@ angular.module('challengeApp.detail', [])
           for(var j=0;j<players.length;j++){
             if(players[j].id === $scope.userChallenges[i].userId){
               $scope.userChallenges[i].player = players[j];
-              console.log()
+              if($scope.userChallenges[i].userId === $scope.loginUser.id){
+                $scope.userIsplaying = true
+              }
               // $scope.accept = $scope.userChallenges[i]
             }
           }
         }
         console.log($scope.userChallenges)
-        console.log($scope.loginUser)
 
       });
     });
