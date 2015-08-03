aa24fc431ddcd18488831f2fd15fa9bd02574a23
Shu Yoshioka <yoshioka63@gmail.com>
2015-08-04 05:58:37 +0900

    [refactor] clean up

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,29 +1,27 @@
 angular.module('challengeApp.profile', ['naif.base64'])
   .controller('ProfileController', ['$scope', '$location', '$state', 'ProfileFactory',
     function($scope, $location, $state, ProfileFactory) {
-      $scope.update = ProfileFactory.upload
+      $scope.update = ProfileFactory.upload;
       $scope.myPic = {};
     }
   ])
 .factory('ProfileFactory', ['UserFactory','$state', function(UserFactory,$state){
-  var returnImage;
   var upload = function(username,userId,newname,image){
     if(newname){
-        console.log('update name')
+        console.log('update name');
         UserFactory.updateUsername(userId, newname);
     }
     if(image){
-      console.log(image)
+      console.log(image);
       UserFactory.uploadImage(image.base64, userId);
     }
     if(newname){
-      $state.go("dashboard", {username: newname});
+      $state.go('dashboard', {username: newname});
     }else if(image){
       $state.reload();
     }
-  }
+  };
   return {
     upload:upload,
   };
-}])
-
+}]);
